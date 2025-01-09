2ecdd346e6985321668a0c27a1d06888eb43f9f9
maddmonkeys <victor.pappis@gmail.com>
2025-01-08 23:13:09 -0800

    Initial commit for QuestLog web app

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button>Login with Google</button>
+    </div>
+  );
+}
+
+export default LoginPage;
